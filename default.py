0de85840f5e39aaac80e08586eda8f6766ae9526
Tommy Jonsson <tommy.jonsson@mac.com>
2014-12-04 23:12:15 +0000

    Stepped version

--- default.py
+++ default.py
@@ -8,7 +8,7 @@ from utils import settings
 __plugin__ = 'Streamcub Library'
 __author__ = 'Streamcub Team'
 __url__ = 'http://www.streamcub.com'
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 print "[PLUGIN] '%s: version %s' initialized!" % (__plugin__, __version__)
 
